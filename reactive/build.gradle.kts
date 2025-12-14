6990cc57213f993793323349abd0d8f01be7b0d2
bee <rkddbtjd123@gmail.com>
2025-12-14 19:49:56 +0900

    build: add kotest dependency

--- reactive/build.gradle.kts
+++ reactive/build.gradle.kts
@@ -4,8 +4,14 @@ plugins {
 
 dependencies {
     implementation("io.projectreactor:reactor-core:3.6.11")
+    testImplementation("io.kotest:kotest-runner-junit5:5.9.1")
+    testImplementation("io.kotest:kotest-assertions-core:5.9.1")
 }
 
 kotlin {
     jvmToolchain(21)
+}
+
+tasks.test {
+    useJUnitPlatform()
 }
