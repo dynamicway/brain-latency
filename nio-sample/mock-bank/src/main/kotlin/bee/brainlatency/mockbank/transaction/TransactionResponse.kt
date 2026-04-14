ba088824764a19e8fe8d3eeebe6b45708d6b819f
bee <rkddbtjd123@gmail.com>
2026-04-14 22:47:35 +0900

    feat: remove unused files

--- nio-sample/mock-bank/src/main/kotlin/bee/brainlatency/mockbank/transaction/TransactionResponse.kt
+++
@@ -1,8 +0,0 @@
-package bee.brainlatency.mockbank.transaction
-
-data class TransactionResponse(
-    val id: Long,
-    val accountId: Long,
-    val description: String,
-    val amount: Double,
-)
