d97a5b8683fdcade8097cd8135047d6fb1ecad0e
bee <rkddbtjd123@gmail.com>
2026-04-14 20:15:40 +0900

    refactor: Improve buildSrc structure

--- gradle/build-logic/kotlin/build.gradle.kts
+++ gradle/build-logic/kotlin/build.gradle.kts
@@ -8,5 +8,5 @@ repositories {
 
 dependencies {
     api(project(":jvm"))
-    implementation("org.jetbrains.kotlin:kotlin-gradle-plugin:${Versions.kotlin}")
+    implementation("org.jetbrains.kotlin:kotlin-gradle-plugin:2.2.21")
 }
--- gradle/build-logic/spring/build.gradle.kts
+++ gradle/build-logic/spring/build.gradle.kts
@@ -8,8 +8,8 @@ repositories {
 
 dependencies {
     api(project(":kotlin"))
-    implementation("org.springframework.boot:spring-boot-gradle-plugin:${Versions.springBoot}")
-    implementation("io.spring.gradle:dependency-management-plugin:${Versions.springDependencyManagement}")
-    implementation("org.jetbrains.kotlin:kotlin-allopen:${Versions.kotlin}")
-    implementation("org.jetbrains.kotlin:kotlin-noarg:${Versions.kotlin}")
+    implementation("org.springframework.boot:spring-boot-gradle-plugin:4.0.5")
+    implementation("io.spring.gradle:dependency-management-plugin:1.1.7")
+    implementation("org.jetbrains.kotlin:kotlin-allopen:2.2.21")
+    implementation("org.jetbrains.kotlin:kotlin-noarg:2.2.21")
 }
