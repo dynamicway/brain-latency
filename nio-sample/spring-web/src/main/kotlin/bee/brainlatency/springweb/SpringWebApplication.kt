d8d994b2a735811a6c22a6543e1a77b5cd8a01df
bee <rkddbtjd123@gmail.com>
2026-04-14 20:42:52 +0900

    feat: Implements spring-web module

--- nio-sample/spring-web/src/main/kotlin/bee/brainlatency/springweb/SpringWebApplication.kt
+++ nio-sample/spring-web/src/main/kotlin/bee/brainlatency/springweb/SpringWebApplication.kt
@@ -2,7 +2,9 @@ package bee.brainlatency.springweb
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import org.springframework.cloud.openfeign.EnableFeignClients
 
+@EnableFeignClients
 @SpringBootApplication
 class SpringWebApplication
 
